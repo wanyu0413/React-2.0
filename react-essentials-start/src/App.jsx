6314c0088fdf752e8cf6c8cefe16b49f60487fd9
Wanyu Jiang <wanyu19930930@gmail.com>
2024-02-01 16:24:29 +0900

    storing components in files

--- react-essentials-start/src/App.jsx
+++ react-essentials-start/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png'
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.pic} alt="..." />
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from './data.js'; //name export
+import Header from './components/Header.jsx'; //default export
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
@@ -41,13 +11,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              pic={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
       </main>
---
+++ react-essentials-start/src/components/CoreConcept.jsx
@@ -0,0 +1,18 @@
+export default function CoreConcept({image, title, description}) {
+    return (
+        <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+}
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
