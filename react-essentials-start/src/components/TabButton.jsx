57a019f39ed2aa284ab650631bf81302a1b0f21c
Wanyu Jiang <wanyu19930930@gmail.com>
2024-02-01 19:29:34 +0900

    reacting to events

--- react-essentials-start/src/components/TabButton.jsx
+++ react-essentials-start/src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log('Heyyyy')
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
