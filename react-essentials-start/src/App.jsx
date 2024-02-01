34d755bf046aee4325333f8f634bbfb9e9c4c1c7
Wanyu Jiang <wanyu19930930@gmail.com>
2024-02-01 19:47:53 +0900

    passing function as value to props

--- react-essentials-start/src/App.jsx
+++ react-essentials-start/src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleClick() {
+    console.log('Heyyyy')
+  }
+
   return (
     <div>
       <Header />
@@ -24,11 +28,12 @@ function App() {
         <section id='examples'>
           <h2>Exampless</h2>
           <menu>
-            <TabButton>Component</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleClick}>Component</TabButton>
+            <TabButton onSelect={handleClick}>JSX</TabButton>
+            <TabButton onSelect={handleClick}>Props</TabButton>
+            <TabButton onSelect={handleClick}>State</TabButton>
           </menu>
+
         </section>
       </main>
     </div>
