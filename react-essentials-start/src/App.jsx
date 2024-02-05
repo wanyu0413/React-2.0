902fd7f559a8c798e938469c1645f834b069d55d
Wanyu Jiang <wanyu19930930@gmail.com>
2024-02-05 17:45:11 +0900

    rendedring content conditionally

--- react-essentials-start/src/App.jsx
+++ react-essentials-start/src/App.jsx
@@ -9,7 +9,7 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
   // selectedTopic => Current state value
   // setSelectedTopic => State updating function
 
@@ -46,13 +46,14 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && <div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
               <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
-          </div>
+          </div>}
         </section>
       </main>
     </div>
