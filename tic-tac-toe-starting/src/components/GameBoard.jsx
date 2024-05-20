0fca1ad697cbbf21d0b8edbba7c29eac88ee2d12
Wanyu Jiang <wanyu19930930@gmail.com>
2024-05-20 19:37:58 +0900

    pass the function back to onClick

--- tic-tac-toe-starting/src/components/GameBoard.jsx
+++ tic-tac-toe-starting/src/components/GameBoard.jsx
@@ -5,22 +5,25 @@ const intialGameBoard = [
 ];
 
 export default function GameBoard() {
-    const [gameBoard, setGameBoard] = useState(intialGameBoard);
+  const [gameBoard, setGameBoard] = useState(intialGameBoard);
 
-    function handleSelectCell(rowIndew, rowIndem) {
-        setGameBoard ((prevGameBoard) => {
-            const updatedGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-            return updatedGameBoard;
+  function handleSelectCell(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      return updatedGameBoard;
     });
+  }
 
   return (
     <ol id="game-board">
-      {intialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((playerSymbol, cellIndex) => (
-              <li key={cellIndex}>
-                <button>{playerSymbol}</button>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectCell(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
