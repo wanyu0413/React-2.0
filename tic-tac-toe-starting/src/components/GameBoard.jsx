8dbb7ed1d57cb8c6a1e9a8d5c1b802598c9fc5ba
Wanyu Jiang <wanyu19930930@gmail.com>
2024-05-20 19:11:32 +0900

    Add game board to App

--- tic-tac-toe-starting/src/components/GameBoard.jsx
+++ tic-tac-toe-starting/src/components/GameBoard.jsx
@@ -1,15 +1,23 @@
 const intialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 export default function GameBoard() {
-    return <ol id="game-board">
-        {intialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-            <ol>
-                {row.map((cell, cellIndex) => <li key={cellIndex}><button></button></li>)}
-            </ol>
-        </li>)}
+  return (
+    <ol id="game-board">
+      {intialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, cellIndex) => (
+              <li key={cellIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
+  );
 }
