750c64d7c73c1305b7d2678467da93ab378f1ff5
Wanyu Jiang <wanyu19930930@gmail.com>
2025-08-10 10:57:24 +0900

    deal with props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,12 +3,12 @@ import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function CoreConcepts(props) {
+function CoreConcepts({title, description, image}) {
   return (
     <li>
-      <img src={props.image} alt="#" />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt="#" />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -39,21 +39,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
+            <CoreConcepts {...CORE_CONCEPTS[0]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
             <CoreConcepts
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
