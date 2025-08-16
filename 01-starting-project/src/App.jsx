1c70e70357793e972e12cde789b2a7ac956d07fe
Wanyu Jiang <wanyu19930930@gmail.com>
2025-08-16 22:27:28 +0900

    props.children

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
   return (
@@ -20,7 +21,16 @@ function App() {
             />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
