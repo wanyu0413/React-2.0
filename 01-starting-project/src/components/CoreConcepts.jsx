814ce9124975e28773300b3757c96b736ccc1375
Wanyu Jiang <wanyu19930930@gmail.com>
2025-08-16 22:05:21 +0900

    saperate components

---
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,11 @@
+function CoreConcepts({title, description, image}) {
+  return (
+    <li>
+      <img src={image} alt="#" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+export default CoreConcepts;
