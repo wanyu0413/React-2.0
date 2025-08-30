3c08cfb43eb5504af2eba0bb2ab1da3b7bf7b020
Wanyu Jiang <wanyu19930930@gmail.com>
2025-08-30 22:16:12 +0900

    set up ternary

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
             <TabButton onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
-          {!selectedTopic && <p>Please select a topic to see an example.</p>}
+          {!selectedTopic && <p>Please select a topic to see an example</p>}
           {selectedTopic && <div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
