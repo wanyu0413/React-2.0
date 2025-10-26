a17d67c566c31a1e5493c527e7085b775bb43943
Wanyu Jiang <wanyu19930930@gmail.com>
2025-10-26 17:51:25 +0900

    create tabs.jsx

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 import { EXAMPLES } from "../data-with-examples.js";
 import TabButton from "./TabButton/TabButton.jsx";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
-    const [selectedTopic, setSelectedTopic] = useState();
+  const [selectedTopic, setSelectedTopic] = useState();
 
-    function handleClick(selectedButton) {
-        // selectedButton => 'Components', 'JSX', 'Props', 'State'
-        setSelectedTopic(selectedButton);
-    }
-    
-    return (
-        <Section title="Examples" id="examples">
-          <menu>
+  function handleClick(selectedButton) {
+    // selectedButton => 'Components', 'JSX', 'Props', 'State'
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        ButtonsContainer="menu"
+        buttons={
+          <>
             <TabButton
               isSelected={selectedTopic === "components"}
               onClick={() => handleClick("components")}
@@ -38,17 +42,20 @@ export default function Examples() {
             >
               State
             </TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a topic to see an example</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </Section>
-    );
+          </>
+        }
+      >
+        {!selectedTopic && <p>Please select a topic to see an example</p>}
+        {selectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
+    </Section>
+  );
 }
---
+++ 01-starting-project/src/components/Tabs.jsx
@@ -0,0 +1,6 @@
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
+    return <>
+    <ButtonsContainer>{buttons}</ButtonsContainer>
+    {children}
+    </>
+}
