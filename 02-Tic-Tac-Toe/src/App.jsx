aea441c74baf40de72c21c10315a7b92deea8b10
Wanyu Jiang <wanyu19930930@gmail.com>
2025-11-01 20:32:42 +0900

    add players and edit btn

--- 02-Tic-Tac-Toe/src/App.jsx
+++ 02-Tic-Tac-Toe/src/App.jsx
@@ -2,11 +2,25 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        PLAYERS
-
+        {/* PLAYERS */}
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
         GAME BOARD
       </div>
-
       LOG
     </main>
   );
