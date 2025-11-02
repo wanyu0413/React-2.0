1ef55d9c3a9c096f49d2fcb54b2dc9ca2f19e8e5
Wanyu Jiang <wanyu19930930@gmail.com>
2025-11-02 23:29:04 +0900

    2 way binding

--- 02-Tic-Tac-Toe/src/components/Player.jsx
+++ 02-Tic-Tac-Toe/src/components/Player.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    // setIsEditing(!isEditing);
+    setIsEditing((Editing) => !Editing);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
   }
 
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    playerName = (
+      <input type="text" required value={name} onChange={handleNameChange} />
+    );
   }
 
   return (
